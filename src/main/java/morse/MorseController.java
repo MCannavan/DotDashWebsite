23bdb9c55cf31fef83ec3ddbeed4f555bf36be2e
mark <mark@mcannavan.dev>
2025-01-30 13:17:46 +0000

    moved business logic into MorseService

--- src/main/java/morse/MorseController.java
+++ src/main/java/morse/MorseController.java
@@ -1,10 +1,5 @@
 package morse;
 
-import dev.mcannavan.dotdash.IMorseTiming;
-import dev.mcannavan.dotdash.MorsePlayer;
-import dev.mcannavan.dotdash.MorseTimingFactory;
-import dev.mcannavan.dotdash.MorseTranslator;
-import org.bouncycastle.util.encoders.Translator;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
 @RestController
 @RequestMapping("/api/morse")
 public class MorseController {
 
+    private final MorseService morseService;
+
+    public MorseController(MorseService morseService) {
+        this.morseService = morseService;
+    }
+
     @GetMapping("/generate")
-    public ResponseEntity<byte[]> playMorse(
+    public ResponseEntity<byte[]> generate(
             @RequestParam(required = false) Float wpm,
             @RequestParam(required = false) Float fwpm,
             @RequestParam(required = false) Float ms,
@@ -30,41 +28,18 @@ public class MorseController {
             @RequestParam(defaultValue = "700") float frequency,
             @RequestParam String morse
             ) {
-        MorsePlayer.MorsePlayerBuilder morsePlayerBuilder = new MorsePlayer.MorsePlayerBuilder();
-        IMorseTiming timing;
-        if (wpm != null) {
-            if(fwpm != null) {
-                timing = MorseTimingFactory.createFarnsworthTimingFromWpm(fwpm,wpm);
-            } else {
-                timing = MorseTimingFactory.createParisTimingFromWpm(wpm);
-            }
-        } else if (ms != null) {
-            if(fms != null) {
-                timing = MorseTimingFactory.createFarnsworthTimingFromMs(fms, ms);
-            } else {
-                timing = MorseTimingFactory.createParisTimingFromMs(ms);
-            }
-        } else {
-            timing = MorseTimingFactory.createParisTimingFromWpm(20);
-        }
-        morsePlayerBuilder.withTiming(timing);
-        morsePlayerBuilder.withFrequency(frequency);
-        MorsePlayer player;
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try {
-            player = morsePlayerBuilder.build();
-            outputStream = player.generateWavFileData(player.generateMorseAudio(morse,volume));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=morse_audio.wav");
         headers.add(HttpHeaders.CONTENT_TYPE, "audio/wav");
 
-        byte[] audioData = outputStream.toByteArray();
+        byte[] audioData = morseService.generate(wpm,fwpm,ms,fms,volume,frequency,morse);
 
         return new ResponseEntity<>(audioData, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/translate")
+    public ResponseEntity<String> translate() {
+        return ResponseEntity.ok("Under Construction, please try again later");
     }
 }
--- src/main/java/morse/MorseService.java
+++ src/main/java/morse/MorseService.java
@@ -3,6 +3,7 @@ package morse;
 import dev.mcannavan.dotdash.*;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @Service
@@ -15,6 +16,38 @@ public class MorseService {
 
     public MorsePlayer getMorsePlayer() {
         return morsePlayer;
+    }
+
+    public byte[] generate(Float wpm, Float fwpm, Float ms, Float fms, int volume, Float frequency, String morse) {
+        MorsePlayer.MorsePlayerBuilder morsePlayerBuilder = new MorsePlayer.MorsePlayerBuilder();
+        IMorseTiming timing;
+        if (wpm != null) {
+            if(fwpm != null) {
+                timing = MorseTimingFactory.createFarnsworthTimingFromWpm(fwpm,wpm);
+            } else {
+                timing = MorseTimingFactory.createParisTimingFromWpm(wpm);
+            }
+        } else if (ms != null) {
+            if(fms != null) {
+                timing = MorseTimingFactory.createFarnsworthTimingFromMs(fms, ms);
+            } else {
+                timing = MorseTimingFactory.createParisTimingFromMs(ms);
+            }
+        } else {
+            timing = MorseTimingFactory.createParisTimingFromWpm(20);
+        }
+        morsePlayerBuilder.withTiming(timing);
+        morsePlayerBuilder.withFrequency(frequency);
+        MorsePlayer player;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            player = morsePlayerBuilder.build();
+            outputStream = player.generateWavFileData(player.generateMorseAudio(morse,volume));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return outputStream.toByteArray();
     }
 
 }
